226e14d66ff1958250a3b5514b660ddf3c35e1eb
isyoo1213 <moethns@gmail.com>
2022-08-29 23:35:40 +0900

    feat: Add features for Primitive Types

---
+++ IntroToTypescript/basics.ts
@@ -0,0 +1,21 @@
+// Primitives : number, String, boolean + null, undefined
+// More comples types : arrays, objects
+// Function type, parameters
+
+// Primitives
+
+let age: number; //대문자 사용 시 JS의 Number객체를 가리킴
+
+age = 12.1; //float형도 가능
+
+let userName : string; //문자형 또한 소문자로 시작
+
+userName = 'insung';
+
+let isInstructor : boolean;
+isInstructor = false;
+
+let hobbies: null;
+// hobbies = 'game';
+
+
