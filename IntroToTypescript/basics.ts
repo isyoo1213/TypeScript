e1604e279f462c98c4a229f5e4f6ab06fdd0933e
isyoo1213 <moethns@gmail.com>
2022-08-29 23:41:54 +0900

    feat: Add features for Arrays and Objects in Typescript

--- IntroToTypescript/basics.ts
+++ IntroToTypescript/basics.ts
@@ -15,7 +15,32 @@ userName = 'insung';
 let isInstructor : boolean;
 isInstructor = false;
 
-let hobbies: null;
-// hobbies = 'game';
+let hobby: null;
+// hobby = 'game';
+
+// More complex types
+
+let hobbies : string[];
+
+hobbies = ['Sports', 'Game'];
+
+let person: {
+  name: string;
+  age: number;
+
+}; 
+//any 타입은 예비적으로 사용되는 타입으로 사용하지 않는 것이 타입스크립트의 취지에 맞음
+
+person = {
+  name: 'insung',
+  age: 32,
+  // isEmployee: true
+}
+
+let people: {
+  name: string;
+  age: number;
+
+}[]; // 객체 배열 
 
 
