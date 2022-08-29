ce635ac7a2b0d0172b689236a55d13c782eb0350
isyoo1213 <moethns@gmail.com>
2022-08-29 23:29:02 +0900

    feat: Add zip file and settings for Typescript

---
+++ IntroToTypescript/with-typescript.js
@@ -0,0 +1,5 @@
+function addWithType(a, b) {
+    return a + b;
+}
+var result = addWithType(2, 5);
+console.log(result);
---
+++ IntroToTypescript/with-typescript.ts
@@ -0,0 +1,8 @@
+function addWithType(a: number, b: number) {
+  return a + b;
+}
+
+let result = addWithType(2, 5);
+// result = addWithType('2', '5');
+
+console.log(result);
